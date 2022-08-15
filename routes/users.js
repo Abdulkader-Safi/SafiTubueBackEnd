b71f65c43505b8e5d84988ced7fd715642e3e5ad
abdulkader-safi <safi.abdulkader@gmail.com>
2022-08-15 12:02:36 +0300

    Verify Token/User verfication && setup basics of user contraller and routes

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,36 @@
 import express from "express";
-import {} from "../contrallers/users.js";
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  update,
+} from "../contrallers/users.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
-// router.get("/test", test);
+// Update user
+router.put("/:id", verifyToken, update);
+
+// delete user
+router.delete("/:id", verifyToken, deleteUser);
+
+// get a user
+router.get("/find/:id", verifyToken, getUser);
+
+// subscribe a user
+router.put("/sub/:channelID", verifyToken, subscribe);
+
+// unsubscribe a user
+router.put("/unsub//:channelID", verifyToken, unsubscribe);
+
+// like a video
+router.put("/like/:videoID", verifyToken, like);
+
+// dislike a video
+router.put("/dislike/:videoID", verifyToken, dislike);
 
 export default router;
