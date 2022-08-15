dad2b131d3911e5dc59c337c35af42ead4562ecf
abdulkader-safi <safi.abdulkader@gmail.com>
2022-08-15 21:51:43 +0300

    VIDEO/updateVideo && VIDEO/deleteVideo

--- contrallers/videos.js
+++ contrallers/videos.js
@@ -23,6 +23,22 @@ export const addVideo = async (req, res, next) => {
 
 export const updateVideo = async (req, res, next) => {
   try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      return next(createError(404, "Video not found!"));
+    }
+    if (req.user.id === video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedVideo);
+    } else {
+      return next(createError(403, "You can update only your video!"));
+    }
   } catch (err) {
     next(err);
   }
@@ -30,6 +46,16 @@ export const updateVideo = async (req, res, next) => {
 
 export const deleteVideo = async (req, res, next) => {
   try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      return next(createError(404, "Video not found!"));
+    }
+    if (req.user.id === video.userId) {
+      await Video.findByIdAndDelete(req.params.id);
+      res.status(200).json("The video has been deleted.");
+    } else {
+      return next(createError(403, "You can delete only your video!"));
+    }
   } catch (err) {
     next(err);
   }
