a510357d6ed4bdaa0062225279f558c19c357b78
abdulkader-safi <safi.abdulkader@gmail.com>
2022-08-15 13:30:54 +0300

    VIDEO/getVideo && VIDEO/addVideo

--- contrallers/videos.js
+++ contrallers/videos.js
@@ -1,7 +1,36 @@
-export const getVideo = async (req, res, next) => {};
+import { createError } from "../error.js";
+import User from "../models/User.js";
+import Video from "../models/Video.js";
 
-export const addVideo = async (req, res, next) => {};
+export const getVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    res.status(200).json(video);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const updateVideo = async (req, res, next) => {};
+export const addVideo = async (req, res, next) => {
+  const newVideo = new Video({ userID: req.user.id, ...req.body });
+  try {
+    const saveVideo = await newVideo.save();
+    res.status(200).json("savedVideo");
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const deleteVideo = async (req, res, next) => {};
+export const updateVideo = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteVideo = async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+};
